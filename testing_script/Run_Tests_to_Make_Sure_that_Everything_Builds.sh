601649284bb588cb32c0404af29460251b07dcb8
Jacob Levernier <jlevern@upenn.edu>
2017-04-13 11:53:06 -0400

    Got PDF checksums to match after realizing that some aux. files were not being copied for the Makefile build.

--- testing_script/Run_Tests_to_Make_Sure_that_Everything_Builds.sh
+++ testing_script/Run_Tests_to_Make_Sure_that_Everything_Builds.sh
@@ -4,10 +4,8 @@
 # Settings
 ########################################
 
-# Get these by running 'sha1sum /path/to/file.pdf' on a rendered PDF from the example files that you've manually inspected.
-sha1sum_of_markdown_based_pdf_output_that_has_been_manually_checked="2cc7cd0ce2901b07ab131064411d5fcb08d49bea"
-
-sha1sum_of_makefile_based_pdf_output_that_has_been_manually_checked="cd5afbaff0d71a345a9dfe9c7863a8e6de46b7a8"
+# Get this by running 'sha1sum /path/to/file.pdf' on a rendered PDF from the example files that you've manually inspected. It should be the same for markdown-based and makefile-based PDF output files.
+sha1sum_of_pdf_output_that_has_been_manually_checked="67a8f4d37a2d53c1cd65f2c07d3c3a12af3a43b0"
 
 ########################################
 # End Settings
@@ -43,6 +41,8 @@ cp --recursive "./latex_files/." "./testing_script/latex_files_for_testing_OK_TO
 
 cp "./builds/temporary_build_files/combined_sections_ready_for_rendering" "./testing_script/latex_files_for_testing_OK_TO_DELETE/latex_files/main.tex"
 
+find "./builds/temporary_build_files/" -name "*.tex" -exec cp {} "./testing_script/latex_files_for_testing_OK_TO_DELETE/latex_files/" \;
+
 cp "./Bibliography.bib" "./testing_script/latex_files_for_testing_OK_TO_DELETE/latex_files/"
 
 echo "Running makefile command..."
@@ -56,10 +56,8 @@ echo "Checksumming the output PDF..."
 makefile_pdf_checksum=$(sha1sum "./main.pdf" | awk '{ print $1 }')
 
 echo -e "The markdown-based PDF checksum is \n     $markdown_to_latex_pdf_checksum"
-echo -e "The expected value (set at the top of this script) is \n     $sha1sum_of_markdown_based_pdf_output_that_has_been_manually_checked"
-
 echo -e "The Makefile-based PDF checksum is \n     $makefile_pdf_checksum"
-echo -e "The expected value (set at the top of this script) is \n     $sha1sum_of_makefile_based_pdf_output_that_has_been_manually_checked"
+echo -e "The expected value for both (set at the top of this script) is \n     $sha1sum_of_pdf_output_that_has_been_manually_checked"
 
 echo "If these are the same, the test has passed."
 
